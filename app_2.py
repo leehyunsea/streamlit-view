b1baba65642691e98f43e07a15d391599d9150ba
leehyunsea <138441050+leehyunsea@users.noreply.github.com>
2023-07-04 15:07:56 +0900

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -1,8 +1,14 @@
 import streamlit as st
+view = [3,2,1]
 st.write('# 경기서부A센터 소모용품 쇼핑몰')
-st.write('## 소모용품 재고파악을 위한 웹사이트입니다.')
-st.write('## 10G 모듈, FAN 등 다양한 물건의 재고를 확인해보세요')
+st.write('## 10G 모듈')
+view
+st.write('## bar chart')
+st.bar_chart(view)
 
+import pandas as pd
+sview = pd.Series(view)
+sview
 
 st.sidebar.title('종류')
 st.sidebar.checkbox('10G 모듈')
