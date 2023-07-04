2bfaff09486d4dd04aa00702976949f867ba7298
leehyunsea <138441050+leehyunsea@users.noreply.github.com>
2023-07-04 15:17:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ st.write('## 다양한 물건의 재고를 확인해보세요')
 
 st.sidebar.title('# 종류')
 st.sidebar.title('1. 10G 모듈')
+st.sidebar.checkbox('15KM')
+st.sidebar.checkbox('40KM')
+st.sidebar.checkbox('80KM')
 st.sidebar.title('2. ')
 st.sidebar.title('3. ')
 st.sidebar.title('4. ')
 st.sidebar.title('5. ')
-st.sidebar.checkbox('10G 모듈')
+
