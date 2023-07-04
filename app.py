d676f0c1bb54fa5ad06b556e0bd015b6b46cb33d
leehyunsea <138441050+leehyunsea@users.noreply.github.com>
2023-07-04 15:14:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
-view = [3,2,1]
 st.write('# 경기서부A센터 소모용품 쇼핑몰')
 st.write('## 10G 모듈')
-view
-st.write('## bar chart')
-st.bar_chart(view)
 
-import pandas as pd
-sview = pd.Series(view)
-sview
 
 st.sidebar.title('종류')
+st.sidebar.title('10G 모듈')
 st.sidebar.checkbox('10G 모듈')
