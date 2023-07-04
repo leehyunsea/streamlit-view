3ff21b809a3e15526f14f84f5c027c19e6ce8310
leehyunsea <138441050+leehyunsea@users.noreply.github.com>
2023-07-04 16:09:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import streamlit as st
-from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
-from  PIL import Image
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import io
 
 st.write('# 경기서부A센터 소모용품 쇼핑몰')
 st.write('## 소모용품 재고파악을 위한 웹사이트입니다.')
@@ -15,14 +7,15 @@ img_url = 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.n
 st.image(img_url)
 
 st.sidebar.title('# 종류')
-with st.sidebar:
-    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning"],
-                         icons=['house', 'camera fill', 'kanban'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
-    }
-    )
+
+st.sidebar.title('1. 10G 모듈')
+st.sidebar.button('15km')
+st.sidebar.button('40km')
+st.sidebar.button('80km')
+
+st.sidebar.title('2. 2.5G 모듈')
+
+
+st.sidebar.title('3. ')
+st.sidebar.title('4. ')
+st.sidebar.title('5. ')
