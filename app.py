4c260f93874444c9395ce8a8fd54bf5579f8fcb2
leehyunsea <138441050+leehyunsea@users.noreply.github.com>
2023-07-04 15:24:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+from PIL import Image
+
 st.write('# 경기서부A센터 소모용품 쇼핑몰')
 st.write('## 소모용품 재고파악을 위한 웹사이트입니다.')
 st.write('## 다양한 물건의 재고를 확인해보세요')
+siba_img = Image.open('siba.jpeg')
+siba_img
 
 st.sidebar.title('# 종류')
 st.sidebar.title('1. 10G 모듈')
