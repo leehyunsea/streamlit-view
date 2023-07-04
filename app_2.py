b334d22dbda3f436e2896e33f4c605d5e2253759
leehyunsea <138441050+leehyunsea@users.noreply.github.com>
2023-07-04 15:06:19 +0900

    Delete app_2.py

--- app_2.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.write('# 경기서부A센터 소모용품 쇼핑몰')
-st.write('## 소모용품 재고파악을 위한 웹사이트입니다.')
-st.write('## 10G 모듈, FAN 등 다양한 물건의 재고를 확인해보세요')
-
-
-st.sidebar.title('# 종류')
-st.sidebar.checkbox('10G 모듈')
